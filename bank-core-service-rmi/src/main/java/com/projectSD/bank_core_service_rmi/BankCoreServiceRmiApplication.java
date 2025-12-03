d30d3154613831bc3dabdd8a78b5e89b0f3ea5b7
Kamal Ennaji <najikamal100@gmail.com>
2025-12-03 19:40:33 +0100

    First push

---
+++ bank-core-service-rmi/src/main/java/com/projectSD/bank_core_service_rmi/BankCoreServiceRmiApplication.java
@@ -0,0 +1,13 @@
+package com.projectSD.bank_core_service_rmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankCoreServiceRmiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankCoreServiceRmiApplication.class, args);
+	}
+
+}
---
+++ bank-core-service-rmi/src/test/java/com/projectSD/bank_core_service_rmi/BankCoreServiceRmiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectSD.bank_core_service_rmi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankCoreServiceRmiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
